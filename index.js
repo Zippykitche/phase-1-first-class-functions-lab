ba4fafc083b9dd353f7b7a95a96d29e96557f7b1
zippykitche <zippyk80@gmail.com>
2024-10-02 23:05:35 +0300

    advanced functions lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fareMultiplier){
+return function(fare){
+return fare * fareMultiplier;
+}
+}
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (drivers, callback) => {
+    return callback(drivers);
+};
